18162f1e427a84ebe380b23e6116e497f1757982
Gustavo Enrique Ruiz Cisneros <enrique.ruiz.cisneros@gmail.com>
2013-11-04 17:41:32 -0700

    Paginacion de articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles
   def index
-    @articles = Article.all
+    @articles = Article.paginate(:page => params[:page], :per_page => 1)
   end
 
 
