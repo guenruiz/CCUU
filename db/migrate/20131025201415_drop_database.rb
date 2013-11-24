1af95b83ee7387b0810559719036da57292406d7
Gustavo Enrique Ruiz Cisneros <enrique.ruiz.cisneros@gmail.com>
2013-11-24 14:55:18 -0700

    pruebas

--- db/migrate/20131025201415_drop_database.rb
+++ db/migrate/20131025201415_drop_database.rb
@@ -1,7 +1,7 @@
 class DropDatabase < ActiveRecord::Migration
   def change
-  	drop_table :articles
-  	drop_table :users
-  	drop_table :sources
+  	#drop_table :articles
+  	#drop_table :users
+  	#drop_table :sources
   end
 end
